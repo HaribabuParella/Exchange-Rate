38e61d4e6c4fdfdf263b64a199e795046c10b7a2
HaribabuParella <parellaharibabu@gmail.com>
2021-10-07 19:32:52 +0530

    Project setup

---
+++ src/test/java/com/assessment/Exchange/Rate/ExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.Exchange.Rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
