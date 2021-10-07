91a3006a52489a09ce7d3486441c08db4b342c46
HaribabuParella <parellaharibabu@gmail.com>
2021-10-07 19:39:08 +0530

    Project setup

--- src/main/java/com/assessment/ExchangeRate/ExchangeRateApplication.java
+++ src/main/java/com/assessment/ExchangeRate/ExchangeRateApplication.java
@@ -1,4 +1,4 @@
-package com.assessment.Exchange.Rate;
+package com.assessment.ExchangeRate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
